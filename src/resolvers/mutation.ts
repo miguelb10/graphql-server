b92be0b2aaea58347b8464e583819768b3b25b58
Miguel Blas <miguel.blas_03@hotmail.com>
2022-08-25 00:12:15 -0500

    add mutation edit and delete

--- src/resolvers/mutation.ts
+++ src/resolvers/mutation.ts
@@ -17,8 +17,66 @@ const mutation: IResolvers = {
         teacher: course.teacher,
         reviews: []
       }
-      database.courses.push(itemCourse);
-      return itemCourse;
+      if (database.courses.filter(item => item.title === itemCourse.title).length === 0) {
+        database.courses.push(itemCourse);
+        return itemCourse;
+      }
+      return {
+        id: '-1',
+        title: 'Course already exist!',
+        description: '',
+        clases: -1,
+        time: 0.0,
+        level: 'ALL',
+        logo: '',
+        path: '',
+        teacher: '',
+        reviews: []
+      }
+    },
+    updateCourse(__: void, { course }): any {
+      const currentElement = _.findIndex(database.courses, function (obj) {
+        return obj.id === course.id
+      });
+      if (currentElement > -1) {
+        const reviews = database.courses[currentElement].reviews;
+        course.reviews = reviews;
+        database.courses[currentElement] = course;
+        return course;
+      }
+
+      return {
+        id: '-1',
+        title: 'Course does not exist!',
+        description: '',
+        clases: -1,
+        time: 0.0,
+        level: 'ALL',
+        logo: '',
+        path: '',
+        teacher: '',
+        reviews: []
+      }
+    },
+    deleteCourse(__: void, { id }): any {
+      const currentElement = _.remove(database.courses, function (obj) {
+        return obj.id === id;
+      });
+      if (currentElement[0] === undefined) {
+        return {
+          id: '-1',
+          title: 'Course does not exist!',
+          description: '',
+          clases: -1,
+          time: 0.0,
+          level: 'ALL',
+          logo: '',
+          path: '',
+          teacher: '',
+          reviews: []
+        }
+      }
+      return currentElement[0];
     }
   }
 }
